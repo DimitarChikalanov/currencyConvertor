f23d64aa900037818a57f205e3bc1297cc6b6b25
danielvichinyan <45153745+danielvichinyan@users.noreply.github.com>
2020-12-12 14:46:41 +0200

    GetMapping route changed a bit

--- src/main/java/com/currency/convertor/controllers/CurrencyConverterController.java
+++ src/main/java/com/currency/convertor/controllers/CurrencyConverterController.java
@@ -5,12 +5,9 @@ import com.currency.convertor.service.CurrencyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-<<<<<<< HEAD:src/main/java/com/currency/convertor/controllers/CurrencyConverterController.java
-=======
 
 import java.util.List;
 import java.util.Map;
->>>>>>> 69db28baf9a7ac3db3b2148e7ea9a6ee1e2662d1:src/main/java/com/currency/convertor/contrroler/Controller.java
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -28,7 +25,7 @@ public class CurrencyConverterController {
         return new ResponseEntity<>(currencyService.convert(model), HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/get_all_currency")
+    @GetMapping("/currencies")
     public List getCurrency() {
         return this.currencyService.getCurrencyRate();
     }
