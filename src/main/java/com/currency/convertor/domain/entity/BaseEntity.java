582fecd34ba6fcbc5b75eab2440d8d3935752405
dimitar <dimitur.chikalanov@gmail.com>
2020-12-14 14:33:28 +0200

    refresh currency

--- src/main/java/com/currency/convertor/domain/entity/BaseEntity.java
+++ src/main/java/com/currency/convertor/domain/entity/BaseEntity.java
@@ -6,13 +6,11 @@ import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
-    private Long id;
-
-    public BaseEntity() {
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     public Long getId() {
         return id;
     }
